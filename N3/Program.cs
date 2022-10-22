55c5d4b6a2fcec474e8ed9aab2a36f1a6bde8448
lolmem745 <93839006+lolmem745@users.noreply.github.com>
2022-10-22 04:24:44 +0300

    Third problem solved

---
+++ N3/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+switch (a)
+{
+    case <= 5:
+        Console.Write("нет");
+        break;
+    case >= 8:
+        Console.Write("введите число меньше 8");
+        break;
+    default:
+        Console.Write("да");
+        break;
+}
