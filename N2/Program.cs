5a5e3f85d3b2d556c274676d11d723ede6e321cc
lolmem745 <93839006+lolmem745@users.noreply.github.com>
2022-10-22 04:17:36 +0300

    Second problem solved

---
+++ N2/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите число: ");
+string a = Convert.ToString(Console.ReadLine());
+int b = Convert.ToInt32(a);
+if (a.Length > 2)
+{
+    for (int i = 0; (i < (a.Length) - 3); i++)
+    {
+        b = b / 10;
+    }
+    b = b % 10;
+    Console.Write(b);
+}
+else
+{
+    Console.Write("Третьей цифры нет");
+}
